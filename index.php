ae4ad22e91040e80d4c31c72c076470276f7d998
Jackymancs4 <jackyman_cs4@live.it>
2016-05-26 09:52:13 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$query = 'scanner+fg';
+$bbb = file_get_contents('https://www.google.it/complete/search?sclient=psy-ab&js=true&site=&source=hp&q='.$query);
+
+print_r($bbb);
+echo "<br>";
+echo "<br>";
+
+$ccc = str_split($bbb);
+
+$pattern = '/\"(.*?)\"/';
+$matches = preg_match_all($pattern, $bbb, $outb);
+
+$pattern = '/\[\".*\",(\[.*\]),{.*}\]/';
+$matches = preg_match_all($pattern, $bbb, $out);
+
+$pattern = '/\"(.*?)\"/';
+$matches = preg_match_all($pattern, $out[1][0], $outa);
+
+print_r($outb[1][0]);
+echo "<br>";
+echo "<br>";
+
+foreach ($outa[1] as $key => $value) {
+  $res=str_replace("\u003cb\u003e", "-->" ,$value);
+  $res=str_replace("\u003c\/b\u003e", "<--" ,$res);
+
+  echo $res."<br>";
+}
