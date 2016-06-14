76a75c6ad7a0b058701ce54b1f2216f039c2187b
Jackymancs4 <jackyman_cs4@live.it>
2016-06-14 21:55:37 +0200

    Linted and indented code.

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ $query = 'scanner+fg';
 $bbb = file_get_contents('https://www.google.it/complete/search?sclient=psy-ab&js=true&site=&source=hp&q='.$query);
 
 print_r($bbb);
-echo "<br>";
-echo "<br>";
+echo '<br>';
+echo '<br>';
 
 $ccc = str_split($bbb);
 
@@ -19,12 +19,12 @@ $pattern = '/\"(.*?)\"/';
 $matches = preg_match_all($pattern, $out[1][0], $outa);
 
 print_r($outb[1][0]);
-echo "<br>";
-echo "<br>";
+echo '<br>';
+echo '<br>';
 
 foreach ($outa[1] as $key => $value) {
-  $res=str_replace("\u003cb\u003e", "-->" ,$value);
-  $res=str_replace("\u003c\/b\u003e", "<--" ,$res);
+    $res = str_replace("\u003cb\u003e", '-->', $value);
+    $res = str_replace("\u003c\/b\u003e", '<--', $res);
 
-  echo $res."<br>";
+    echo $res.'<br>';
 }
